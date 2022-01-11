66fc4f28877566938d13cec747a4d284a368a60d
intrusionfinance <97505036+intrusionfinance@users.noreply.github.com>
2022-01-11 15:16:27 +0800

    Create UnhackToken.sol

---
+++ UnhackToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract UnhackToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("unHACK", "unHACK") {
+        uint256 totalSupply = 100_000_000_000 * 10 ** 18;
+        _mint(msg.sender, totalSupply);
+    }
+}
